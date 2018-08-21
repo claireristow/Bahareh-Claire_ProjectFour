f64bf3ecc6a7efedf8d4a67b7c7d1afc3e964baa
Bahareh <bahareh.adham@gmail.com>
2018-08-21 17:30:15 -0400

    end of tuesday

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,60 @@
-const hello = 'hello';
+// randomly generate letters on a 4x4 grid 
+// each time the user clicks a letter, that letter will appear in answer div at the bottom of the screen
+
+
+// then user can submit answer div text to API
+
+// if text ="true", then score + 1
+// if text = "false", then score doesn't change
+
+const app = {};
+
+const chars = 'abcdefghijklmnopqrstuvwxyz';
+
+//randomly generate letters on the board
+app.getBoard = function(){
+    // generate random letters
+    const ranNum = Math.floor(Math.random() * 26 ) +11;
+    const ranLet = Math.toString(ranNum);
+    
+    
+    // append them to the board
+}
+
+
+
+
+
+
+
+
+
+app.userInput = $('p').text();
+
+app.url = 'https://www.dictionaryapi.com/api/v1/references/collegiate/xml/';
+
+app.key = '8c5c85a3-ffa3-4f09-b901-7db8209015dc';
+
+function getWord() {
+    return $.ajax({
+        url: `${app.url}${app.userInput}`,
+        method: 'GET',
+        data: {
+            key: app.key
+        }
+    })
+}
+
+
+app.init = function () {
+    // app.getWord();
+    app.getBoard();
+    console.log('hi')
+}
+
+$(function () {
+    app.init();
+
+});
+
+
